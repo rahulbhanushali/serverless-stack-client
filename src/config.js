bf1aa6ce376ada313ac7bd9560a44953509afd3f
Rahul Bhanushali <imrahul@amazon.com>
2020-07-01 17:56:50 +0530

    First commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+        BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://ixayr57beg.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_Eemf6U6wM",
+        APP_CLIENT_ID: "50qutvsjugl27o36g5kqineqo4",
+        IDENTITY_POOL_ID: "us-east-1:db315873-b60d-453e-82a9-5a035993d857"
+    }
+};
